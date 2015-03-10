b067c6aa59e394b45867b627c84cfa90eca8b07d
Michael Siddi <msiddi@vivocha.com>
2015-03-10 15:51:49 +0100

    added custom engagement, weblead example, cbn example

--- VivochaSDKExample/src/com/vivocha/sdkexample/DetailActivity.java
+++ VivochaSDKExample/src/com/vivocha/sdkexample/DetailActivity.java
@@ -1,9 +1,16 @@
 package com.vivocha.sdkexample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.vivocha.sdk.Vivocha;
+import com.vivocha.sdk.Vivocha.VivochaContactCreationCallback;
+import com.vivocha.sdk.VivochaContact;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -11,7 +18,92 @@ public class DetailActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+	
 		
+		Button customEngagement = (Button) findViewById(R.id.customEngagement);
+		Button buttonWebLead = (Button) findViewById(R.id.buttonWeblead);
+		Button buttonCBN = (Button) findViewById(R.id.buttonCBN);
+
+		
+		if (customEngagement != null) {
+			customEngagement.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					if(Vivocha.getContact() != null){
+						//there is already a contact in progress
+						
+						Vivocha.getContact().showView(true);
+					}
+					else{
+						//there is no contact... let's create one!
+						
+						Vivocha.createChat(Vivocha.getDataCollection(), new VivochaContactCreationCallback() {
+							
+							@Override
+							public void onCreationSuccess(VivochaContact contact) {
+								if(contact != null){
+									contact.connect();
+									contact.showView(true);
+								}
+							}
+							
+							@Override
+							public void onCreationFailure() {
+								//Aww, snap! Show an alert to notify the failure
+							}
+						});
+					}
+				}
+			});
+		}
+		
+		if (buttonWebLead != null) {
+			buttonWebLead.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					//we don't need to check for existing webleads because the only type of contact that can remain 'alive' is the chat one
+					Vivocha.createWeblead(Vivocha.getDataCollection(), "email@example.com", new VivochaContactCreationCallback() {
+						
+						@Override
+						public void onCreationSuccess(VivochaContact contact) {
+							//WebLead created!
+						}
+						
+						@Override
+						public void onCreationFailure() {
+							//There was an error creating the weblead!
+						}
+					});
+				}
+			});
+		}
+		
+		if (buttonCBN != null) {
+			buttonCBN.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					Vivocha.createCallBackNow(Vivocha.getDataCollection(), "+1555123456", new VivochaContactCreationCallback() {
+						
+						@Override
+						public void onCreationSuccess(VivochaContact contact) {
+							//CBN created!
+						}
+						
+						@Override
+						public void onCreationFailure() {
+							//There was an error creating the CallBackNow!
+						}
+					});
+					
+				}
+			});
+		}
 	}
 
 	@Override
--- VivochaSDKExample/src/com/vivocha/sdkexample/MainActivity.java
+++ VivochaSDKExample/src/com/vivocha/sdkexample/MainActivity.java
@@ -1,5 +1,8 @@
 package com.vivocha.sdkexample;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +13,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.vivocha.sdk.Vivocha;
+import com.vivocha.sdk.Vivocha.VivochaCustomActionCallback;
+import com.vivocha.sdk.VivochaUtils;
+import com.vivocha.sdk.VivochaValues;
+import com.vivocha.sdk.internal.managers.VivochaNotificationManager;
+import com.vivocha.sdk.internal.managers.VivochaNotificationManager.VivochaNotificationManagerCallbacks;
+import com.vivocha.sdk.model.VivochaCustomAction;
+import com.vivocha.sdk.model.datacollection.VivochaDataCollection;
+import com.vivocha.sdk.model.datacollection.VivochaDataCollectionField;
+import com.vivocha.sdk.model.datacollection.VivochaDataCollectionForm;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,8 +31,70 @@ public class MainActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_main);
 		
 		
-		//Vivocha Start
+		//Create a service inside the Vivocha Agent Desktop Settings area
+	    //Be sure to set the right bundle identifier (e.g. In this project the bundle id is com.vivocha.sdkesample)
+		
+	    //You will find a string like this one in your Android Service Settings
         Vivocha.start(this, "YOUR_ACCOUNT_ID", "YOUR_SERVICE_ID");
+
+        //if you want to use your custom engagement button uncomment the following line
+        Vivocha.setBlockSideButton(true); //this line disables the automatic side tab
+        
+        
+        //During a contact you can send an action from the Agent Desktop
+        //using !action|TestAction|[{"activity":"detail"}]
+        
+        Vivocha.manager().bindCustomAction("TestAction", new VivochaCustomActionCallback() {
+			
+			@Override
+			public void onAction(VivochaCustomAction action) {
+				
+				if(action != null){
+					JSONObject data = (JSONObject) VivochaUtils.getObjectFromJSONArray((JSONArray) action.actionData, 0);
+					
+					String which = VivochaUtils.getJSONString(data, "activity");
+					
+					if(which != null){
+						if (which.equalsIgnoreCase("detail")) {
+							doDetailAction();
+						}
+					}					
+				}
+			}
+		});
+        
+        
+        //Set a data collection
+        
+        VivochaDataCollectionForm formOne = new VivochaDataCollectionForm();
+        formOne.name = "My Form Title";
+        formOne.description = "My Form Description";
+        
+        formOne.addField(new VivochaDataCollectionField("Field Title", VivochaValues.VivochaDataCollectionFieldTypeNumber, "NumberValue -e.g 3.99", "Field Description", true, false));
+        formOne.addField(new VivochaDataCollectionField("AnotherField Title", VivochaValues.VivochaDataCollectionFieldTypeText, "TextValue", "AnotherField Description", true, false));
+
+        //Set a nick name for your user (It will be shown on the agent desktop when a contact is requested)
+        formOne.addField(new VivochaDataCollectionField("Nickname", VivochaValues.VivochaDataCollectionFieldTypeNickname, "User Nickname", "", true, false));
+        
+        VivochaDataCollection dc = new VivochaDataCollection();
+        dc.addForm(formOne);
+
+        Vivocha.setDataCollection(dc);
+        
+        //If you want to enable Push notifications
+        VivochaNotificationManager.manager().register("YOUR_GOOGLE_API_KEY", new VivochaNotificationManagerCallbacks() {
+			
+			@Override
+			public void onSuccess(String registrationID) {
+				//save your registration ID
+			}
+			
+			@Override
+			public void onFailure() {
+				
+			}
+		}, true); //True if you want to display a popup that request the user to install Google Play Services
+
 
 		Button openDetailButton =  (Button) findViewById(R.id.buttonDetail);
 		openDetailButton.setOnClickListener(new OnClickListener() {
